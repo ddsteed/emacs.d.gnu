f9a78a8eee1a355a93be955f509c6a36329fd12e
ddsteed <ddsteed@gmail.com>
2025-03-18 14:47:04 +0800

    add early-init

---
+++ early-init.el
@@ -0,0 +1,46 @@
+;;; early-init.el --- Emacs 27+ pre-initialisation config
+
+;;; Commentary:
+
+;; Emacs 27+ loads this file before (normally) calling
+;; `package-initialize'.  We use this file to suppress that automatic
+;; behaviour so that startup is consistent across Emacs versions.
+
+;;; Code:
+
+;; 启动的时候先不加载各种 package，等真需要的时候再加
+(setq package-enable-at-startup nil)
+
+;; If an `.el' file is newer than its corresponding `.elc', load the `.el'.
+(setq load-prefer-newer t)
+
+;; Set Garbage Collection threshold to 1GB during startup. `gcmh' will clean things up later.
+(setq gc-cons-threshold 1073741824
+      gc-cons-percentage 0.6)
+
+;; Write any customizations to a temp file so they are discarded.
+(setq custom-file (make-temp-file "custom-" nil ".el"))
+
+;; Faster to disable these here (before they've been initialized)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(scroll-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+;; Give the frame basic coloring while waiting for the theme to load. The main purpose of this is to not blind me when it's dark by flashing a screen full of white. These colors are from doom-one.
+(set-face-attribute 'default nil :background "#282c34" :foreground "#bbc2cf")
+;; Default frame settings. This is actually maximized, not full screen.
+(push '(fullscreen . maximized) initial-frame-alist)
+(push '(ns-transparent-titlebar . t) default-frame-alist)
+
+;; Resizing the Emacs frame can be a terribly expensive part of changing the font. By inhibiting this, we easily halve startup times with fonts that are larger than the system default.
+(setq frame-inhibit-implied-resize t
+      frame-resize-pixelwise t)
+
+;; Ignore X resources; its settings would be redundant with the other settings in this file and can conflict with later config (particularly where the cursor color is concerned).
+(advice-add #'x-apply-session-resources :override #'ignore)
+
+;; So we can detect this having been loaded
+(provide 'early-init)
+
+;;; early-init.el ends here
